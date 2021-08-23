91b04fd742ce411ba8f74264912652fc14993ae5
eliel <eliel.niukkanen@medisapiens.fi>
2021-08-23 08:56:53 +0300

    Add Kubernetes

--- blog/client/src/PostList.tsx
+++ blog/client/src/PostList.tsx
@@ -15,7 +15,7 @@ export default () => {
   const [posts, setPosts] = useState<Posts>({})
 
   const fetchPosts = async () => {
-    const res = await axios.get('http://localhost:4002/posts')
+    const res = await axios.get('http://posts.com/posts')
     setPosts(res.data)
   }
 
