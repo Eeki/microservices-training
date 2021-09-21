a74ffb75ad89b952ecc8e9610cc248987c184eac
eliel <eliel.niukkanen@medisapiens.fi>
2021-09-21 20:16:52 +0300

    continue tinkering with nats

--- nats-test/README.md
+++ nats-test/README.md
@@ -9,3 +9,10 @@ kubectl get pods
 # 2. Do the port forwarding
 kubectl port-forward nats-depl-XXX-XXX 4222:4222
 ```
+
+***Note!***
+Nats streaming server monitoring is running in `<hostname>:8222/streaming`
+e.g: http://localhost:8222/streaming
+
+Some detail information for the subscriptions e.g:
+http://localhost:8222/streaming/channelsz?subs=1
--- nats-test/src/listener.ts
+++ nats-test/src/listener.ts
@@ -1,15 +1,31 @@
 import nats, { Message } from 'node-nats-streaming'
+import { randomBytes } from 'crypto'
 
 console.clear()
 
-const stan = nats.connect('ticketing', '123', {
+const stan = nats.connect('ticketing', randomBytes(4).toString('hex'), {
   url: 'http://localhost:4222',
 })
 
 stan.on('connect', () => {
   console.log('Listener connected to NATS')
 
-  const subscription = stan.subscribe('ticket:created')
+  stan.on('close', () => {
+    console.log('NATS connection closed!')
+    process.exit()
+  })
+
+  const options = stan
+    .subscriptionOptions()
+    .setManualAckMode(true)
+    .setDeliverAllAvailable()
+    .setDurableName('my-durable-name')
+
+  const subscription = stan.subscribe(
+    'ticket:created',
+    'my-queue-group-name',
+    options,
+  )
 
   subscription.on('message', (msg: Message) => {
     const data = msg.getData()
@@ -17,5 +33,10 @@ stan.on('connect', () => {
     if (typeof data === 'string') {
       console.log(`Received event #${msg.getSequence()}, with data: ${data}`)
     }
+
+    msg.ack()
   })
 })
+
+process.on('SIGINT', () => stan.close())
+process.on('SIGTERM', () => stan.close())
